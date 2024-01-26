15171e7d65874ab950cb41382871982a67569bd3
William McPherson <willmcpherson2@gmail.com>
2024-01-26 15:31:31 +1100

    Move WebSocket connection setup to system

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,16 +8,9 @@ use shared::{ClientMessage, ServerMessage};
 use web_sys::WebSocket;
 
 fn main() {
-    let ws = WebSocket::new("ws://localhost:3000/ws").unwrap();
-    ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
-    let inbox = Inbox::new(&ws);
-    let outbox = Outbox::new(&ws);
-
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(inbox)
-        .insert_non_send_resource(outbox)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, connect))
         .add_systems(Update, update)
         .run();
 }
@@ -26,10 +19,7 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut outbox: NonSendMut<Outbox>,
 ) {
-    outbox.send(ClientMessage::RequestClientId);
-
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Circle::new(4.0).into()),
         material: materials.add(Color::WHITE.into()),
@@ -55,6 +45,17 @@ fn setup(
         transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn connect(world: &mut World) {
+    let ws = WebSocket::new("ws://localhost:3000/ws").unwrap();
+    ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
+    let inbox = Inbox::new(&ws);
+    let mut outbox = Outbox::new(&ws);
+    outbox.send(ClientMessage::RequestClientId);
+
+    world.insert_resource(inbox);
+    world.insert_non_send_resource(outbox);
 }
 
 fn update(mut commands: Commands, inbox: ResMut<Inbox>) {
